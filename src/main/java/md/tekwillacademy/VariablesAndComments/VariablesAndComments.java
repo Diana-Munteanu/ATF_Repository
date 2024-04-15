05849bc0bd1955c66a8ee3e574885e2abbca06b4
Diana Munteanu <diana.munteanu@enabld.tech>
2024-04-15 13:26:51 +0300

    added 16 primitive data type variables as a practical exercise

---
+++ src/main/java/md/tekwillacademy/VariablesAndComments/VariablesAndComments.java
@@ -0,0 +1,56 @@
+package md.tekwillacademy.VariablesAndComments;
+
+public class VariablesAndComments {
+
+    public static void main(String[] args) {
+        boolean isJavaFun = true;
+        boolean isCourseComplete = false;
+
+        char myGrade = 'A';
+        char myFavoriteLetter = 'D';
+
+        byte numberOfStudents = 25;
+        byte numberOfEmployees = 100;
+
+        short distanceInMeters = 1000;
+        short numberOfBooks = 500;
+
+        int numDevelopersPerTeam = 10;
+        int numLinesOfCodeWritten = 10000;
+
+        long populationOfCountry = 1500000000L;
+        long distanceToSunInKm = 149600000L;
+
+        float piValue = 3.14f;
+        float temperatureInCelsius = 36.6f;
+
+        double priceOfItem = 19.99;
+        double distanceToNearestStarInLightYears = 4.37;
+
+        // Printing each variable
+        System.out.println("Is Java fun? " + isJavaFun);
+        System.out.println("Is Course complete? " + isCourseComplete);
+
+        System.out.println("My grade: " + myGrade);
+        System.out.println("My favorite letter: " + myFavoriteLetter);
+
+        System.out.println("Number of students: " + numberOfStudents);
+        System.out.println("Number of employees: " + numberOfEmployees);
+
+        System.out.println("Distance in meters: " + distanceInMeters);
+        System.out.println("Number of books: " + numberOfBooks);
+
+        System.out.println("Number of developers per team: " + numDevelopersPerTeam);
+        System.out.println("Number of lines of code written: " + numLinesOfCodeWritten);
+
+        System.out.println("Population of country: " + populationOfCountry);
+        System.out.println("Distance to sun in km: " + distanceToSunInKm);
+
+        System.out.println("Value of pi: " + piValue);
+        System.out.println("Temperature in Celsius: " + temperatureInCelsius);
+
+        System.out.println("Price of item: " + priceOfItem);
+        System.out.println("Distance to nearest star in light years: " + distanceToNearestStarInLightYears);
+    }
+
+}
